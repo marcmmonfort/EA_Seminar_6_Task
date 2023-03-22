b94f71d5c46dbb474539f59468fc6b4d19a5a233
Quim Moran M <marc.moran@estudiantat.upc.edu>
2023-03-22 19:41:53 +0100

    FrontEnd y Server.

---
+++ Demo_Angular_Project_FrontEnd/FrontEnd_Angular/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'app_component', component: AppComponent },  
+  { path: 'about_component', component: AboutComponent },
+  { path: '',   redirectTo: '/app_component', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
